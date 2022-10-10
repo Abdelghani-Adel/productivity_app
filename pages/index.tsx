d153a34668716dfb7cac4739913d990f32c15099
Abdelghani <abdelghani.private@yahoo.com>
2022-10-10 17:40:29 +0200

    changed some styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import AddDiaryBtn from "../components/Buttons/AddDiaryBtn";
 import AddHabitBtn from "../components/Buttons/AddHabitBtn";
 import AddQuoteBtn from "../components/Buttons/AddQuoteBtn";
 import AddTodoBtn from "../components/Buttons/AddTodoBtn";
-import styles from "../styles/Home.module.css";
 import ButtonGroup from "@mui/material/ButtonGroup";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <ButtonGroup variant="outlined">
         <AddDiaryBtn />
         <AddQuoteBtn />
