d52bf56a65bcf9fbceae4f35b72e3d01476f35f3
AbdelghaniAdel <abdelghani.private@yahoo.com>
2022-10-11 05:31:57 +0200

    Prepared Redux Store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import "../styles/globals.css";
 import "../styles/NewDiary.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import store from "../store/store";
 import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
---
+++ store/store.ts
@@ -0,0 +1,18 @@
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    // Reducers will be added here
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+
+export default store;
